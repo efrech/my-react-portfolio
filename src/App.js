330d0ada99824f0bf7d21185cf5facb9aa019e4a
Evelyn Frech <evelynmariafrech@gmail.com>
2022-06-02 23:54:09 -0400

    modify resume routes

--- src/App.js
+++ src/App.js
@@ -6,6 +6,7 @@ import About from './pages/About';
 import Navigation from './components/Navigation';
 import Contact from './pages/Contact';
 import Portfolio from './pages/Portfolio';
+import Resume from './components/Resume';
 import Header from './components/Header';
 import Footer from './components/Footer';
 
@@ -14,29 +15,33 @@ const routerBaseName = process.env.PUBLIC_URL;
 function App() {
   return (
     <Router basename={routerBaseName}>
-        <div className="flex-column justify-flex-start min-100-vh">
-          <Navigation />
-          <Header />
-          <div className="container">
-             {/* Navigational routes */}
-            <Routes>
-              <Route 
-                path="/" 
-                element={<About />} 
-              />
-              <Route 
-                path="/contact" 
-                element={<Contact />} 
-              />
-                <Route 
-                path="/portfolio" 
-                element={<Portfolio />} 
-              />
-            </Routes>
-          </div>
-          <Footer />
+      <div className="flex-column justify-flex-start min-100-vh">
+        <Navigation />
+        <Header />
+        <div className="container">
+          {/* Navigational routes */}
+          <Routes>
+            <Route
+              path="/"
+              element={<About />}
+            />
+            <Route
+              path="/contact"
+              element={<Contact />}
+            />
+            <Route
+              path="/portfolio"
+              element={<Portfolio />}
+            />
+            <Route
+              path="/resume"
+              element={<Resume />}
+            />
+          </Routes>
         </div>
-      </Router>
+        <Footer />
+      </div>
+    </Router>
   );
 }
 
